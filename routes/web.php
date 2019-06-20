b8f320aec0c5f00a8b7f232d7fa618c83d089da9
Yonathan Bohorquez <ycbohorquez@ucundinamarca.edu.co>
2019-06-20 12:54:06 -0500

    Implementación de plantilla ADMIN LTE

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'InicioController@index');
 
-Route::get('permiso/{nombre}', 'PermisoController@index'); 
+Route::get('permiso/{nombre}', 'PermisoController@index');
